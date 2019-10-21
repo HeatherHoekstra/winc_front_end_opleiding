418e34e6f7ded2fd8656f15e395096568e582bb9
Heather Hoekstra <heatherhoekstra@upcmail.nl>
2019-10-21 16:39:38 +0200

    pokemon werkt!

--- Week 3/Dag 4/w3d4-2/script.js
+++ Week 3/Dag 4/w3d4-2/script.js
@@ -4,6 +4,8 @@ const jijBent = document.getElementById("ik");
 const knop2 = document.getElementById("knop2");
 const anderIs = document.getElementById("ander");
 const fightbutton = document.getElementById("fight");
+let points = 0;
+let points2 = 0;
 restart.style.display = "none";
 anderIs.style.display = "none";
 restart.style.display = "none";
@@ -29,6 +31,7 @@ knop.addEventListener("click", event => {
     naam.innerHTML = pokemonName;
     const img = document.getElementById("img");
     img.innerHTML = `<img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name.id}.png" height= "200px">`;
+    points = parseInt(name.base_experience);
   };
 });
 
@@ -51,32 +54,41 @@ knop2.addEventListener("click", event => {
     naam.innerHTML = pokemonName;
     const img = document.getElementById("img2");
     img.innerHTML = `<img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name.id}.png" height= "200px">`;
+    points2 = parseInt(name.base_experience);
   };
 });
 
 //fight Werkt niet :'(
+//fightbutton.addEventListener("click", event => {
+// getYourRandomPokemon().then(responseJSON => {
+//   const pokemon = responseJSON;
+
+//   const getPokemonPoints = points => {
+//     points = parseInt(points.base_experience);
+//     getPokemonPoints(pokemon);
+//     console.log("hoi");
+//   };
+
+//   getEnemyRandomPokemon().then(responseJSON => {
+//     const pokemon2 = responseJSON;
+
+//     restart.style.display = "block";
+//     const getPokemon2 = points2 => {
+//       points2 = parseInt(points2.base_experience);
+//       getPokemon2(pokemon2);
+//       console.log(points, points2);
+//       uitslag(points, points2);
+//     };
+//   });
+// });
+//});
 fightbutton.addEventListener("click", event => {
-  getYourRandomPokemon().then(responseJSON => {
-    const pokemon = responseJSON;
-    getPokemon(pokemon);
-    const getpokemon = points => {
-      const punten = points.base_experience;
-    };
-    getEnemyRandomPokemon().then(responseJSON => {
-      const pokemon2 = responseJSON;
-      getPokemon2(pokemon2);
-      restart.style.display = "block";
-      const getPokemon2 = points2 => {
-        const punten2 = points2.base_experience;
-        const uitslag = document.getElementById("uitslag");
-        if (punten < punten2) {
-          uitslag.innerHTML = `Helaas, je hebt verloren! Jouw pokémon had ${points} punten, en je tegenstander had er ${points2}!`;
-        } else if (punten == punten2) {
-          uitslag.innerHTML = `Gelijkspel! Beide pokémon hadden ${points} punten! `;
-        } else {
-          uitslag.innerHTML = `Hoera, je hebt gewonnen! Jouw pokémon had ${points} punten, en je tegenstander had er ${points2}! `;
-        }
-      };
-    });
-  });
+  document.getElementById("uitslag");
+  if (points < points2) {
+    uitslag.innerHTML = `Helaas, je hebt verloren! Jouw pokémon had ${points} punten, en je tegenstander had er ${points2}!`;
+  } else if (points == points2) {
+    uitslag.innerHTML = `Gelijkspel! Beide pokémon hadden ${points} punten! `;
+  } else {
+    uitslag.innerHTML = `Hoera, je hebt gewonnen! Jouw pokémon had ${points} punten, en je tegenstander had er ${points2}! `;
+  }
 });
